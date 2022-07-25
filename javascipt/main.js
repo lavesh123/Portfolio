c75178d323ed2694bb5d752730e6817ccf749616
Lovesh <73507728+lavesh123@users.noreply.github.com>
2022-07-26 03:00:56 +0530

    Add files via upload

---
+++ javascipt/main.js
@@ -0,0 +1,12 @@
+let nav =document.querySelector('.navbar');
+window.onscroll = function (){
+    if(document.documentElement.scrollTop>20){
+        nav.classList.add("header-scrolled");
+    } else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse=document.querySelector(".navbar-collapse.collapse");
